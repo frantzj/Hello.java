c480fb50c6b083d23010a21136b1c54359c140cc
frantzj <46724689+frantzj@users.noreply.github.com>
2019-01-15 14:01:47 -0500

    Add files via upload
    1/15/19 Jordan Frantz Lab 1 CMSC 255-002

---
+++ hello.java
@@ -0,0 +1,21 @@
+// Hello.java //
+// Lab 1 //
+// Jordan Frantz //
+// 1/15/19 //
+// CMSC 255 - 002 // 
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Jordan Frantz");
+		System.out.println("Lab 1");
+		System.out.println("Printing out the program");
+		System.out.println("1/15/19");
+		System.out.println("CMSC 255 - 002");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
